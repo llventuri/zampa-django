f97ee4cfca27dcbfff15ec9c29cb1f82319a8575
llventuri <llventuri30@gmail.com>
2026-06-23 16:26:53 +0200

    static-root

--- zampa/settings_prod.py
+++ zampa/settings_prod.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "/var/www/zampa.online/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
